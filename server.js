9f530d0822958f2ae3c8528ed44005659c91b950
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-03-23 12:20:59 +0100

    dev: added serve-index

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express");
+const serveIndex = require("serve-index");
 
 const app = express();
 const port = 3000;
+const www = "./public";
 
 app.use((req, res, next) => {
   console.log("req.url", req.url);
   next();
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.static(www));
+app.use(serveIndex(www));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
