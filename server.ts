40f4337552504f7386def216a29fbb54d000eac7
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-03-23 16:27:49 +0100

    dev: added Router

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import serveIndex from "serve-index";
+import { api } from "./api";
 
 const app = express();
 const port = 3000;
@@ -10,9 +11,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/api/date", (req, res) => {
-  res.json({ date: new Date() });
-});
+app.use("/api", api);
 
 app.use(express.static(www));
 app.use(serveIndex(www, { icons: true }));
