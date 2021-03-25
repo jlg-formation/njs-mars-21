c1a537cb2b978b2bc83e9cecd0d02afe8da916cb
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-03-25 15:09:52 +0100

    test: template of all rest test

--- test/rest.spec.ts
+++ test/rest.spec.ts
@@ -6,11 +6,13 @@ import {WebServer} from '../src/WebServer';
 const debug = dbg('njs:back:rest.spec.ts');
 
 const port = +(process.env.TEST_PORT || 3333);
-const uri =
+const mongoUri =
   process.env.TEST_MONGO_URI || 'mongodb://localhost:27017/gestion-stock';
 
+const url = `http://localhost:${port}/api/articles`;
+
 describe('REST', () => {
-  const server = new WebServer(port, uri);
+  const server = new WebServer(port, mongoUri);
   before(async () => {
     // start a server on port 3333
     await server.start();
@@ -23,12 +25,20 @@ describe('REST', () => {
     console.log('stop with success');
   });
 
+  it('should remove all the articles', async () => {});
+  it('should create an article', async () => {});
+  it('should create another article', async () => {});
   it('should get all the articles', async () => {
-    const response = await fetch(`http://127.0.0.1:${port}/api/articles`);
+    const response = await fetch(url);
     const status = response.status;
     const articles = await response.json();
     debug('articles: ', articles);
     assert.ok(articles instanceof Array);
     assert.strictEqual(status, 200);
+    assert.strictEqual(articles.length, 5);
   });
+  it('should rewrite the first article', async () => {});
+  it('should update the first article', async () => {});
+  it('should update all the article', async () => {});
+  it('should delete one article', async () => {});
 });
