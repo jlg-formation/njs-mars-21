a38775cb7c2c987e5baff495f84d86e1dc87f4a7
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-03-26 16:24:25 +0100

    dev: added ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'gestion-truc',
+      script: 'build/src/index.js',
+      watch: 'build',
+      env: {
+        NODE_ENV: 'development',
+        TEST_DB_URI: 'file',
+        DB_MONGOOSE: undefined,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        TEST_DB_URI: 'mongodb://localhost:27017/gestion-stock',
+        DB_MONGOOSE: 'mongoose',
+      },
+    },
+  ],
+};
