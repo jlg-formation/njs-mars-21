602218aab559208a9f650e62e8e3aa48b615542d
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-03-26 16:27:37 +0100

    dev: added ovh conf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,6 +14,11 @@ module.exports = {
         TEST_DB_URI: 'mongodb://localhost:27017/gestion-stock',
         DB_MONGOOSE: 'mongoose',
       },
+      env_ovh: {
+        NODE_ENV: 'production',
+        TEST_DB_URI: 'file',
+        DB_MONGOOSE: undefined,
+      },
     },
   ],
 };
