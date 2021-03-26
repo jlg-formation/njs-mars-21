749d7a28950430eb131c8f960e19d34b91be3ca6
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-03-26 13:58:24 +0100

    dev: added currency formatter

--- src/frontend.ts
+++ src/frontend.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import {DbServer} from './dbserver/DbServer';
 
+function currency(price: number): string {
+  return price.toFixed(2);
+}
+
 export const frontend = (db: DbServer) => {
   const app = express.Router();
 
   app.get('/', (req, res) => {
     (async () => {
       const articles = await db.retrieveAll();
-      res.render('pages/index', {articles: articles});
+      res.render('pages/index', {articles, currency});
     })();
   });
 
