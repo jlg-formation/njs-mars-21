bc755f45d9e7cbdfe256a992003e4ba36d66c690
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-03-26 15:11:21 +0100

    dev: added selection

--- public/main.js
+++ public/main.js
@@ -5,3 +5,26 @@ window.getDetails = function (id) {
   console.log('id: ', id);
   window.location = `/details/${id}`;
 };
+
+const selectedArticleIds = new Set();
+
+window.toggle = function (id) {
+  console.log('toggle id: ', id);
+  if (selectedArticleIds.has(id)) {
+    selectedArticleIds.delete(id);
+  } else {
+    selectedArticleIds.add(id);
+  }
+  refreshArticles();
+};
+
+function refreshArticles() {
+  const trs = document.querySelectorAll('table tbody tr');
+  trs.forEach(tr => {
+    if (selectedArticleIds.has(tr.id)) {
+      tr.classList.add('selected');
+    } else {
+      tr.classList.remove('selected');
+    }
+  });
+}
